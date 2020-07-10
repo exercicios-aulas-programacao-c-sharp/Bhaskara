48c9a719b5f5e24114c9ba19904a8a191c1c0d05
Diego Neri <diego@diegoneri.com>
2020-07-10 17:56:00 -0300

    Adiciona projeto base dotnet

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bhaskara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
